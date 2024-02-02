00c35d29eb7a02e3067e527983d3f59a1a0dc4d7
xuanranran <juanshengyuan@gmail.com>
2024-02-02 17:18:34 +0800

    Update onlineupdate.sh

--- onlineupdate.sh
+++ onlineupdate.sh
@@ -88,10 +88,11 @@ format_choose () {
     if [ $machine_num == 3 ];then
         echo -e '\e[91mX86固定使用Squashfs格式\e[0m'
         break
-    else
-    if [ $machine_num == 4 ];then
-        echo -e '\e[91mX86固定使用Squashfs格式\e[0m'
-        break
+        fi
+        if [ $machine_num == 4 ];then
+            echo -e '\e[91mX86固定使用Squashfs格式\e[0m'
+            break
+        fi
     else
         #echo -e '\e[92m根据数字选择固件格式或退出\e[0m'
         #echo -e '0 --- 退出\n1 --- Ext4\n2 --- Squashfs'
