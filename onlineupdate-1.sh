c0774f70418438121486019e7032b6de142c5052
LovinYarn <juanshengyuan@gmail.com>
2025-03-19 15:40:15 +0800

    Update onlineupdate-1.sh

--- onlineupdate-1.sh
+++ onlineupdate-1.sh
@@ -4,5 +4,6 @@
 # System Required: Linux
 #=================================================
 cd /tmp
+mount -t tmpfs -o remount,size=100% tmpfs /tmp
 wget https://github.com/xuanranran/OpenWRT-X86_64/releases/latest/download/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz
 gunzip immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz && sysupgrade -F immortalwrt-x86-64-generic-squashfs-combined-efi.img
